8a8c937db9243bf133d29b19f87f4e5f7ef1c47d
Kevin Roussety <kevinroussety1@gmail.com>
2026-03-06 15:28:18 +0100

    Added code for cd and pwd

--- miniShell.c
+++ miniShell.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#define lenInput 100
+#include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+
+/*** 
+pwd() retrieves the current working directory into a PATH_MAX-sized buffer using getcwd() and prints it to stdout as "Current working directory: ". If getcwd() fails it prints "Cannot get current working directory path" to stderr and returns. 
+***/
+void pwd()
+{
+    // Buffer to store the current working directory path
+    char buffer[PATH_MAX];
+    // Attempt to retrieve the current working directory
+    if (getcwd(buffer, PATH_MAX) == NULL)
+    {
+        // If getcwd fails, print an error message to stderr
+        fprintf(stderr, "Cannot get current working directory path\n");
+        return;
+    }
+    printf("Current working directory: %s\n", buffer);
+
+}
 
 int main()
 {
-    char userInput[lenInput];
-    char lastChar;
+    // Buffer that stores the raw command entered by the user
+    char userInput[PATH_MAX];
+    // lastChar: used to detect '&' for background execution
+    // command: first token of the user input (the command itself)
+    // argument: second token (argument to the command)
+    char lastChar, *command, *argument;
+    // Flag indicating whether the command should run in background
     bool backgroundFlag = false;
+    // Stores the length of the user input string
     int stringLen = 0;
 
     printf("Input your command.\n");
@@ -16,25 +42,31 @@ int main()
 
     while (1)
     {
+        // Display the shell prompt
         printf(">>>: ");
 
-        if (fgets(userInput, lenInput, stdin) == NULL)
+        // Read user input from stdin
+        if (fgets(userInput, PATH_MAX, stdin) == NULL)
         {
+            // Exit loop if input fails
             break;
         }
-
+        // Determine length of the input string
         stringLen = strlen(userInput);
 
+        // If input is empty, skip to next iteration
         if (stringLen == 0)
         {
             continue;
         }
 
-        // Remove the nextine(\n) in the command from the user
+        // Remove the nextine(\n) in the command added by fgets
         userInput[stringLen - 1] = '\0';
         stringLen--;
+        // Store the last character of the input (used to detect '&')
         lastChar = userInput[stringLen - 1];
 
+        //The program is terminated if the user input either exit or EXIT
         if (strcmp(userInput, "exit") == 0 || strcmp(userInput, "EXIT") == 0)
         {
             break;
@@ -45,10 +77,16 @@ int main()
         {
             backgroundFlag = true;
             userInput[stringLen - 1] = '\0';
-            stringLen--;
         }
 
-        if (strcmp(userInput, "?") == 0)
+        // Split the user input into tokens using space as delimiter
+        // First token is the command
+        command = strtok(userInput, " ");
+        argument = strtok(NULL, " ");
+
+        // Compare the command entered by the user with supported commands
+        //We use string compare to check what the command the user has inputted
+        if (strcmp(command, "?") == 0)
         {
             printf("\nHelp Menu:\n");
             printf("- Prefix external commands with \"!\"\n");
@@ -57,21 +95,36 @@ int main()
             printf("- cd,\n- pwd,\n- mkdir,\n- exit,\n- jobs\n");
         }
 
-        else if (strstr(userInput, "cd"))
+        else if (strcmp(command, "cd") == 0)
         {
+            //Check if the user has not inserted a path, if so the program output an error
+            if (argument == NULL)
+            {
+                printf("Error: cd require a path\n");
+            }
+            //Execute the change directory method and also check for error
+            else if (chdir(argument) != 0)
+            {
+                perror("cd failed");
+                continue;
+            }
+            //If there is no error we output the new working directory
+            pwd();
         }
 
-        else if (strstr(userInput, "pwd"))
+        else if (strcmp(command, "pwd") == 0)
         {
+            pwd();
         }
 
-        else if (strstr(userInput, "mkdir"))
+        else if (strcmp(command, "mkdir") == 0)
         {
         }
-        else if (strstr(userInput, "jobs"))
+        else if (strcmp(command, "jobs") == 0)
         {
         }
-        else {
+        else
+        {
             printf("Unknown command. Use '?' for help or '!' for external commands\n");
         }
     }
